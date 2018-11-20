fb554591ccb13de971f1f92a872456c862e6111e
Daniel Barclay <daniel@urbanintel.io>
2018-11-20 14:25:13 +0000

    Bug fix that required ReduxDevTools to be installed to load app

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ const rootReducer = combineReducers({
     historicData: historicDataReducer
 });
 
-const composeEnhancers = process.env.NODE_ENV ==='development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV ==='development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
