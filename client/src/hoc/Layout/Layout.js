ed6e05d1e2ce3f745bea0c6f8aeea32bcf5ae3c7
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2018-11-20 11:44:02 +0000

    Solve conflict

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../../components/Navbar/Navbar.js';
+import Navbar from '../../components/Navbar/Navbar';
 
 class Layout extends React.Component {
     state= {};
--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,17 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 
-  module.exports = function override(config, env) {
-  config = injectBabelPlugin(
-     ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
-     config,
-   );
-    return config;
-  };
+const rootImportConfig = [
+  "root-import",
+  {
+    rootPathPrefix:"",
+    rootPathPrefix:"client/src",
+  }
+]
+
+module.exports = function override(config, env) {
+config = injectBabelPlugin(
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    config,
+  );
+  return config;
+};
