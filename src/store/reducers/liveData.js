0e070265938783e6229e2e389d621734838a9069
Daniel Barclay <barclaysd@me.com>
2018-11-21 18:32:59 +0000

    Error and loading state added for all actions called in containers to allow state of Redux dispatch call to be dynamically rendered to use

--- src/store/reducers/liveData.js
+++ src/store/reducers/liveData.js
@@ -3,21 +3,23 @@ import {updateObject} from "../utility";
 
 const initialState = {
     data: [],
-    loading: false
+    loading: false,
+    error: false
 };
 
 const liveDataFetchSuccess = (state, action) => {
     return updateObject(state, {
         data: action.tempData,
-        loading: false
+        loading: false,
+        error: false
     })
 };
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case(actionTypes.FETCH_LIVE_DATA_START): return updateObject(state, {loading: true});
+        case(actionTypes.FETCH_LIVE_DATA_START): return updateObject(state, {loading: true, error: false});
         case(actionTypes.FETCH_LIVE_DATA_SUCCESS): return liveDataFetchSuccess(state, action);
-        case(actionTypes.FETCH_LIVE_DATA_FAILED): return updateObject(state, {loading: false});
+        case(actionTypes.FETCH_LIVE_DATA_FAILED): return updateObject(state, {loading: false, error: true});
         default: return state;
     }
 };
