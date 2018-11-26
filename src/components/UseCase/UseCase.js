bad9a4ab4f5e9795a1c9966e79147954641f656b
Daniel Barclay <barclaysd@me.com>
2018-11-26 15:40:20 +0000

    fixed a bug where the url would change but the component wouldnt render and cleaned the imports

--- src/components/UseCase/UseCase.js
+++ src/components/UseCase/UseCase.js
@@ -5,14 +5,13 @@ import { Menu, Icon } from 'antd';
 import {Link} from 'react-router-dom';
 import UseCaseRouter from '../../routers/UseCaseRouter';
 
+
 class UseCase extends React.Component {
 
     navLinkOnClick = (e) => {
-        console.log('Link clicked',e)
+        console.log('Link clicked',e);
     };
-
     render() {
-
         return (
             <div className={styles.UseCase}>
                 <Row gutter={15} >
@@ -21,19 +20,19 @@ class UseCase extends React.Component {
                             <Menu onClick={this.navLinkOnClick} style={{fontSize: '24px'}} mode="inline">
 
                                 <Menu.Item key="info">
-                                    <Link to={this.props.match.url + "/Information"}><span><Icon type="notification" /><span>Information</span></span></Link>
+                                    <Link to={this.props.match.url + "/information"}><span><Icon type="notification" /><span>Information</span></span></Link>
                                 </Menu.Item>
 
                                 <Menu.Item key="link">
-                                    <Link to={this.props.match.url + "/Connections"}><span><Icon type="link" /><span>Connections</span></span></Link>
+                                    <Link to={this.props.match.url + "/connections"}><span><Icon type="link" /><span>Connections</span></span></Link>
                                 </Menu.Item>
 
                                 <Menu.Item key="readings">
-                                    <Link to={this.props.match.url + "/Readings"}><span><Icon type="dot-chart" /><span>Readings</span></span></Link>
+                                    <Link to={this.props.match.url + "/readings"}><span><Icon type="dot-chart" /><span>Readings</span></span></Link>
                                 </Menu.Item>
 
                                 <Menu.Item key="historic-data">
-                                    <Link to={this.props.match.url + "/HistoricData"}><span><Icon type="pie-chart" /><span>Historic Data</span></span></Link>
+                                    <Link to={this.props.match.url + "/historicData"}><span><Icon type="pie-chart" /><span>Historic Data</span></span></Link>
                                 </Menu.Item>
                             </Menu>
                         </div>
--- src/routers/UseCaseRouter.js
+++ src/routers/UseCaseRouter.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import {BrowserRouter, Switch, Route, withRouter} from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import Info from '../components/UseCase/Information/Information'
 import Conn from '../components/UseCase/Connections/Connections'
 import Read from '../components/UseCase/Readings/Readings'
 import Hist from '../components/UseCase/HistoricData/HistoricData'
 
 
-const UseCaseRouter = (props) => (
+const UseCaseRouter = (props) => {
+    console.log(props.url + "/information")
+    return (
 
-    <BrowserRouter>
             <Switch>
                 <Route exact path={props.url + "/"} component={Info} />
-                <Route path={props.url + "/Information"} component={Info} />
-                <Route path={props.url + "/Connections"}  component={Conn}/>
-                <Route path={props.url + "/Readings"}  component={Read}/>
-                <Route path={props.url + "/HistoricData"}  component={Hist}/>
+                <Route exact path={props.url + "/information"} component={Info} />
+                <Route exact path={props.url + "/connections"}  component={Conn}/>
+                <Route path={props.url + "/readings"}  component={Read}/>
+                <Route path={props.url + "/historicData"}  component={Hist}/>
             </Switch>
 
-    </BrowserRouter>
-
-
-);
+)};
 
 export default UseCaseRouter;
