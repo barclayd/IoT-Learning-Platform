cfcc3371fffaf2dcb9c640e8a6b1b713ebb31417
FaisalJulaidan <Julaidan.Faisal@gmail.com>
2018-11-27 12:48:29 +0000

    Fix some bugs

--- src/store/reducers/arduinoData.js
+++ src/store/reducers/arduinoData.js
@@ -4,7 +4,7 @@ import {updateObject} from "../utility";
 const initialState = {
     data: [],
     loading: false,
-    error: false
+    error: true
 };
 
 const arduinoDataFetchSuccess = (state, action) => {
@@ -17,7 +17,7 @@ const arduinoDataFetchSuccess = (state, action) => {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case(actionTypes.FETCH_ARDUINO_DATA_START): return updateObject(state, {loading: true, error: false});
+        case(actionTypes.FETCH_ARDUINO_DATA_START): return updateObject(state, {loading: true});
         case(actionTypes.FETCH_ARDUINO_DATA_SUCCESS): return arduinoDataFetchSuccess(state, action);
         case(actionTypes.FETCH_ARDUINO_DATA_FAILED): return updateObject(state, {loading: false, error: true});
         default: return state;
