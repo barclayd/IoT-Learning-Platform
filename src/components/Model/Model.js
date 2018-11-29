7321c82f069b4b0216b0cab175a7388d2a304ee6
Daniel Barclay <barclaysd@me.com>
2018-11-29 14:46:38 +0000

    Add aceessiblity features

--- src/components/Model/Model.js
+++ src/components/Model/Model.js
@@ -5,7 +5,7 @@ import styles from './Model.module.scss'
 class Model extends Component {
     render() {
         return (
-            <main className={styles.Model}>
+            <main role="main" className={styles.Model}>
                 {this.props.children}
             </main>
         )
