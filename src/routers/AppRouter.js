bd4c37024f87fe1e2f73509e00bc1c0742e447f2
Daniel Barclay <barclaysd@me.com>
2018-11-29 22:25:42 +0000

    Community sign in, integrated auth with react router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,42 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, withRouter, Redirect} from 'react-router-dom';
 import UseCase from '../components/UseCase/UseCase';
 import UseCasesList from '../containers/UseCasesList/UseCasesList';
 import Auth from "../containers/Auth/Auth";
 import Logout from '../containers/Auth/Logout/Logout';
+import {connect} from 'react-redux';
 
 const AppRouter = (props) => {
-    return (
-        <React.Fragment>
+    let routes = (
+        <Switch>
+            <Route exact path="/" component={UseCasesList}/>
+            <Route exact path="/login" component={Auth}/>
+            <Route exact path="/logout" component={Logout}/>
+            <Redirect to='/'/>
+        </Switch>
+    );
+
+    if(props.isAuthenticated){
+        routes =
             <Switch>
                 <Route exact path="/" component={UseCasesList}/>
                 <Route exact path="/login" component={Auth}/>
                 <Route exact path='/logout' component={Logout} />
                 <Route path="/usecases/:id" render={props => <UseCase {...props} />} />
+                <Redirect to='/'/>
             </Switch>
+    }
 
+    return (
+        <React.Fragment>
+            {routes}
         </React.Fragment>
 )};
 
-export default AppRouter;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+};
+
+export default withRouter(connect(mapStateToProps)(AppRouter));
