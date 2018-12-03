26f604001c79358718cae4167accc6e58ac9501f
Daniel Barclay <daniel@urbanintel.io>
2018-12-03 15:30:32 +0000

    All use case data is now requested from Firebase

--- src/components/UseCase/UseCase.js
+++ src/components/UseCase/UseCase.js
@@ -10,60 +10,65 @@ import {connect} from 'react-redux';
 class UseCase extends React.Component {
 
     navLinkOnClick = (e) => {
-        console.log('Link clicked',e);
+        console.log('Link clicked', e);
 
     };
 
     render() {
         const useCases = this.props.useCases;
-        let useCase;
-        for(let i = 0; i < useCases.length; i++){
-            if(useCases[i].id === this.props.match.params.id){
-                useCase = useCases[i];
+            let useCase;
+            for (let i = 0; i < useCases.length; i++) {
+                if (useCases[i].id === this.props.match.params.id) {
+                    useCase = useCases[i];
+                }
             }
-        }
-        console.log(useCase);
-        return (
-            <div aria-label={'Use Cases'} className={styles.UseCase}>
-                <Row gutter={15} >
-                    <Col span={6}>
-                        <div className={styles.Sider}>
-                            <Menu onClick={this.navLinkOnClick} style={{fontSize: '24px'}} mode="inline">
+            console.log(useCase);
+            return (
+                <div aria-label={'Use Cases'} className={styles.UseCase}>
+                    <Row gutter={15}>
+                        <Col span={6}>
+                            <div className={styles.Sider}>
+                                <Menu onClick={this.navLinkOnClick} style={{fontSize: '24px'}} mode="inline">
 
-                                <Menu.Item key="info">
-                                    <Link to={this.props.match.url + "/information"}><span><Icon type="notification" aria-label={'Link to Information'}/><span>Information</span></span></Link>
-                                </Menu.Item>
+                                    <Menu.Item key="info">
+                                        <Link to={this.props.match.url + "/information"}><span><Icon type="notification"
+                                                                                                     aria-label={'Link to Information'}/><span>Information</span></span></Link>
+                                    </Menu.Item>
 
-                                <Menu.Item key="link">
-                                    <Link to={this.props.match.url + "/connections"}><span><Icon type="link" aria-label={'Link to Connections'}/><span>Connections</span></span></Link>
-                                </Menu.Item>
+                                    <Menu.Item key="link">
+                                        <Link to={this.props.match.url + "/connections"}><span><Icon type="link"
+                                                                                                     aria-label={'Link to Connections'}/><span>Connections</span></span></Link>
+                                    </Menu.Item>
 
-                                <Menu.Item key="readings">
-                                    <Link to={this.props.match.url + "/readings"}><span><Icon type="dot-chart" aria-label={'Link to Readings'}/><span>Readings</span></span></Link>
-                                </Menu.Item>
+                                    <Menu.Item key="readings">
+                                        <Link to={this.props.match.url + "/readings"}><span><Icon type="dot-chart"
+                                                                                                  aria-label={'Link to Readings'}/><span>Readings</span></span></Link>
+                                    </Menu.Item>
 
-                                <Menu.Item key="historic-data">
-                                    <Link to={this.props.match.url + "/historicData"}><span><Icon type="pie-chart" aria-label={'Link to Historic Data'}/><span>Historic Data</span></span></Link>
-                                </Menu.Item>
-                            </Menu>
-                        </div>
-                    </Col>
+                                    <Menu.Item key="historic-data">
+                                        <Link to={this.props.match.url + "/historicData"}><span><Icon type="pie-chart"
+                                                                                                      aria-label={'Link to Historic Data'}/><span>Historic Data</span></span></Link>
+                                    </Menu.Item>
+                                </Menu>
+                            </div>
+                        </Col>
 
-                    <Col span={18}>
-                        <div className={styles.Content}>
-                            <UseCaseRouter url={this.props.match.url} useCase={useCase}/>
-                        </div>
-                    </Col>
-                </Row>
-            </div>
+                        <Col span={18}>
+                            <div className={styles.Content}>
+                                <UseCaseRouter url={this.props.match.url} useCase={useCase}/>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
 
-        )
+            )
     }
 }
 
 const mapStateToProps = state => {
     return {
-        useCases: state.useCaseData.useCases,
+        // useCases: state.useCaseData.useCases,
+        useCases: state.useCaseFirebase.data
     }
 };
 
