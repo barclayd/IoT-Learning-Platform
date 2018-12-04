81c53cce15c207543243629bda3261ddc1c55add
Daniel Barclay <barclaysd@me.com>
2018-12-04 11:26:32 +0000

    Styled form, enabled selection options and multiple selection

--- src/containers/Settings/Settings.js
+++ src/containers/Settings/Settings.js
@@ -42,27 +42,54 @@ class Settings extends Component {
         });
 
         let emailSettings = Object.keys(emails).map((email) => {
-            return <FormItem {...formItemLayout} key={email} label={email}> <Input defaultValue={emails[email]} /> </FormItem>
+            console.log(email);
+            switch (email) {
+                        case('senders'):
+                            return <FormItem {...formItemLayout} key={email} label={email}>
+                                {/*<Input defaultValue={emails[email]} />*/}
+                                <Select mode='multiple' placeholder='Please select email addresses' defaultValue={emails[email]}>
+                                    <Option value='default'>{emails[email]}</Option>
+                                    <Option value='email1'>test@gmail.com</Option>
+                                    <Option value='fahrenheit'>peter.trott@gmail.com</Option>
+                                </Select>
+                            </FormItem>;
+                        default:
+                            return <FormItem {...formItemLayout} key={email} label={email}> <Input defaultValue={emails[email]} /> </FormItem>
+
+            }
         });
 
         let sensorsSettings = sensors.map((sensor) => {
             return Object.keys(sensor).map((setting) => {
-                console.log(parseInt(sensor[setting]));
-                if(parseInt(sensor[setting]) !== undefined) {
-                    return (<span key={setting}>
-                        <FormItem {...formItemLayout} label={setting}>
+                if(!isNaN(parseInt(sensor[setting]))) {
+                    return (
+                        <FormItem {...formItemLayout} label={setting} key={sensor[setting]}>
                             <InputNumber defaultValue={sensor[setting]} style={{ width: '65%', marginRight: '3%' }}/>
-                             <Select style={{ width: '32%' }}>
+                            <Select style={{ width: '32%' }} defaultValue='celsius'>
                                 <Option value='celsius'>°C</Option>
                                 <Option value='fahrenheit'>°F</Option>
                             </Select>
                         </FormItem>
-                    </span>)
+                    )
                 } else {
-                    return <FormItem {...formItemLayout} key={setting} label={setting}> <Input defaultValue={sensor[setting]} style={{ width: '100%' }}/> </FormItem>
+                    switch (setting) {
+                        case('sensorName'):
+                            return (
+                                <FormItem {...formItemLayout} label={setting} key={sensor[setting]}>
+                                <Select defaultValue={sensor[setting]}>
+                                    <Option value={sensor[setting]}>{sensor[setting]}</Option>
+                                    <Option value='motion'>Motion Sensor</Option>
+                                </Select>
+                            </FormItem>);
+                        default:
+                            return <FormItem {...formItemLayout} key={setting} label={setting}> <Input
+                                defaultValue={sensor[setting]} style={{width: '100%'}}/>
+                            </FormItem>
+                    }
                 }
             });
         });
+
 
         let button = <Button type="primary" htmlType="submit">Submit</Button>;
 
