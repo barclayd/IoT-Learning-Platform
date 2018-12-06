3a49944dbf9e861753a00eca2e4986d6ab6ae959
Daniel Barclay <barclaysd@me.com>
2018-12-06 18:51:54 +0000

    Users with the role of trainer can see Add new use case card

--- src/containers/UseCasesList/UseCasesList.js
+++ src/containers/UseCasesList/UseCasesList.js
@@ -3,8 +3,9 @@ import UseCaseCard from "../../components/UseCaseCard/UseCaseCard";
 import styles from './UseCasesList.module.scss'
 import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
-import { Row, Col } from 'antd';
+import {Row, Col, Card, Icon} from 'antd';
 import {Link} from 'react-router-dom';
+const { Meta } = Card;
 
 
 class UseCasesList extends Component {
@@ -16,17 +17,17 @@ class UseCasesList extends Component {
 
     render() {
         let useCases = this.props.useCases.map((useCase, index) => {
-                    if(useCase.access.listedUsers !== null) {
-                        if(useCase.access.listedUsers.includes(localStorage.getItem("userId")) || (useCase.access.listedUsers.includes(this.props.userId))) {
-                            return <Col key={index} span={8} >
-                                <Link to={"/usecases/" + (index)}>
-                                    <UseCaseCard isLoading={this.props.loading} {...useCase} aria-label={'Use case cards list'}/>
-                                </Link>
-                            </Col>
-                        } else {
-                            return null
-                        }
+                if(useCase.access.listedUsers !== null) {
+                    if(useCase.access.listedUsers.includes(localStorage.getItem("userId")) || (useCase.access.listedUsers.includes(this.props.userId))) {
+                        return <Col key={index} span={8} >
+                            <Link to={"/usecases/" + (index)}>
+                                <UseCaseCard isLoading={this.props.loading} {...useCase} aria-label={'Use case cards list'}/>
+                            </Link>
+                        </Col>
+                    } else {
+                        return null
                     }
+                }
             }
         );
 
@@ -34,6 +35,24 @@ class UseCasesList extends Component {
 
         const printedUseCases = (useCases.every(arrayIsEmpty)) ? <p>No use cases are currently linked to your account. Please contact your trainer.</p> : useCases;
 
+        let addNewUseCaseCard;
+        if(this.props.loading) {
+            addNewUseCaseCard = <Card bordered={true} loading={true} aria-label={'Use case card is loading'}></Card>
+        } else {
+            addNewUseCaseCard = <Card
+                hoverable
+                bordered={true}
+                style={{ width: 300}}
+                bodyStyle= {{ minHeight: 150}}
+                cover={<img height='150px' alt='add new usecase' src='/images/add-new.png' />}
+                actions={[<Icon type="check-circle" theme="twoTone" style={{fontSize:'22px'}} aria-label={'experiment icon'}/>]}
+                aria-label={`Use case card, Use case name is add new use case card, Use case description is click here to add a new use case`}
+            >
+                <div>
+                    <Meta title='Add a New Usecase' description='Click on this card to add a new use case. Here you can set all properties for a new use case' aria-label='Add a new usecase card'/>
+                </div>
+            </Card>
+        }
 
 
         return (
@@ -41,6 +60,7 @@ class UseCasesList extends Component {
                 <Row gutter={16}>
                     {printedUseCases}
                 </Row>
+                {localStorage.getItem("role") === 'Trainer' ? addNewUseCaseCard : null}
             </div>
 
         )
