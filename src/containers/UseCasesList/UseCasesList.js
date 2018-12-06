b234fec33a696447ebbb04aed9baf3b22697f0f6
Daniel Barclay <barclaysd@me.com>
2018-12-06 19:53:16 +0000

    Added modal to display new use case form

--- src/containers/UseCasesList/UseCasesList.js
+++ src/containers/UseCasesList/UseCasesList.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
 import {Row, Col, Card, Icon} from 'antd';
 import {Link} from 'react-router-dom';
+import AddNewUseCase from '../../containers/AddNewUseCase/AddNewUseCase';
 const { Meta } = Card;
 
 
@@ -35,32 +36,13 @@ class UseCasesList extends Component {
 
         const printedUseCases = (useCases.every(arrayIsEmpty)) ? <p>No use cases are currently linked to your account. Please contact your trainer.</p> : useCases;
 
-        let addNewUseCaseCard;
-        if(this.props.loading) {
-            addNewUseCaseCard = <Card bordered={true} loading={true} aria-label={'Use case card is loading'}></Card>
-        } else {
-            addNewUseCaseCard = <Card
-                hoverable
-                bordered={true}
-                style={{ width: 300}}
-                bodyStyle= {{ minHeight: 150}}
-                cover={<img height='150px' alt='add new usecase' src='/images/add-new.png' />}
-                actions={[<Icon type="check-circle" theme="twoTone" style={{fontSize:'22px'}} aria-label={'experiment icon'}/>]}
-                aria-label={`Use case card, Use case name is add new use case card, Use case description is click here to add a new use case`}
-            >
-                <div>
-                    <Meta title='Add a New Usecase' description='Click on this card to add a new use case. Here you can set all properties for a new use case' aria-label='Add a new usecase card'/>
-                </div>
-            </Card>
-        }
-
 
         return (
             <div className={styles.UseCasesList} aria-label={`Select a use case`}>
                 <Row gutter={16}>
                     {printedUseCases}
                 </Row>
-                {localStorage.getItem("role") === 'Trainer' ? addNewUseCaseCard : null}
+                <AddNewUseCase />
             </div>
 
         )
