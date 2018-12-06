fd12434a87edda30edd4b3a46010de19ca7fea9f
Daniel Barclay <daniel@urbanintel.io>
2018-12-06 17:01:28 +0000

    Cleaned code ahead of testing to make debugging easier

--- src/components/UseCase/Connections/Connections.js
+++ src/components/UseCase/Connections/Connections.js
@@ -3,7 +3,6 @@ import styles from './Connections.module.scss';
 
 
 const Connections = (props) => {
-    console.log("Render Connections...");
     return (
        <div className={styles.Connections}>
        <h1>Connections</h1>
