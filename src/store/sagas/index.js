4d92e4baa4557ff510dbccfa4a9d39a6377c1a7c
Daniel Barclay <daniel@urbanintel.io>
2018-12-06 16:54:00 +0000

    Removed old useCase (websockets) action creators, reducer and saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -2,7 +2,6 @@ import {takeEvery, all, takeLatest} from 'redux-saga/effects';
 import * as actionTypes from '../actions/actionTypes';
 import {fetchDataSaga} from './historicData';
 import {fetchLiveDataSaga} from './liveData';
-import {fetchUseCaseSaga} from "./useCaseData";
 import {fetchArduinoDataSaga} from './arduinoData';
 import {fetchUseCaseDataSaga, updateUseCaseSaga, submitSettingsSaga} from './useCaseFirebase'
 import {fetchUsersDataSaga} from './usersFirebase';
@@ -25,10 +24,6 @@ export function* watchHistoricData() {
 
 export function* watchLiveData() {
     yield takeEvery(actionTypes.INIT_FETCH_LIVE_DATA, fetchLiveDataSaga);
-}
-
-export function* watchUseCaseData() {
-    yield takeEvery(actionTypes.INIT_FETCH_USECASE, fetchUseCaseSaga);
 }
 
 export function* watchArduinoData() {
