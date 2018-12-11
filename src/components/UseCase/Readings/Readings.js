146697d62383bc7bae8b0208257735758b6eb18e
Daniel Barclay <barclaysd@me.com>
2018-12-11 15:48:35 +0000

    Dynamic tooltips based on Application state/state of props e.g. loading, error

--- src/components/UseCase/Readings/Readings.js
+++ src/components/UseCase/Readings/Readings.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import styles from './Readings.module.scss';
 import TemperatureData from '../../../containers/TemperatureData/TemperatureData';
+import {Icon, Tooltip} from "antd";
+import * as text from "../../../assets/staticText";
 
 
 const Readings = (props) => {
-    
+    const questionMarkStyle = {position: 'absolute', fontSize: '30px', right: '30px', top: '20px'};
+
+
     return (
        <div className={styles.Information}>
            <React.Fragment>
+               {/*<Tooltip title={(localStorage.getItem("email") !== null) ? text.toolbarLoggedIn : text.toolbarLoggedOut} placement="bottom">*/}
+                   {/*<Icon type="question-circle" theme="filled" style={questionMarkStyle} defaultVisible={true}/>*/}
+               {/*</Tooltip>*/}
                <TemperatureData ID={props.match.params.id}/>
            </React.Fragment>
        </div>
