e80b930d167017fe2b6011e8347c454bcc72f29d
Daniel Barclay <barclaysd@me.com>
2018-12-12 11:09:40 +0000

    Change connection page name

--- src/components/UseCase/Connections/Connections.js
+++ src/components/UseCase/Connections/Connections.js
@@ -5,7 +5,7 @@ import styles from './Connections.module.scss';
 const Connections = (props) => {
     return (
        <div className={styles.Connections}>
-       <h1>Connections</h1>
+       <h1>Configure Your Arduino</h1>
        <p>The breadboard for {props.useCaseData.arduinoName}</p>
        <img className={styles.ArduinoImg} alt="arduino" src={"/images/" + props.useCaseData.arduino} width="650px" aria-label={ props.useCaseData.arduino} />
        <p>{props.useCaseData.arduinoDesc}</p>
